fef71d1c4922b60a295c2d823781537a1a28ab05
SEBASTIAN NIEBLAS ZAZUETA <sebas_nit_040899@hotmail.com>
2018-08-17 07:45:05 -0700

    Cambios

---
+++ MetodosEstaticos/Matematicas.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodosEstaticos
+{
+    class Matematicas
+    {
+        public static int Potencia(int numeroBase, int exponente)
+        {
+            int resultado = 1;
+
+            for (int i = 0; i < exponente; i++)
+            {
+                resultado = resultado * numeroBase;
+            }
+
+            return resultado;
+        }
+    }
+}
